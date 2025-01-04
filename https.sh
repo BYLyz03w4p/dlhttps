26dcf7585df30813efaa8a35fc5cc4ad08f21b81
BYLyz03w4p <lujunxi@msn.com>
2025-01-04 10:32:25 +0800

    Update https.sh

--- https.sh
+++ https.sh
@@ -5,32 +5,30 @@ if [[ $EUID -ne 0 ]]; then
    echo "Error: This script must be run as root!" 1>&2
    exit 1
 fi
-
 timedatectl set-timezone Asia/Shanghai
-
-# 生成随机用户名和密码
+# 生成随机用户名、密码和端口
 username=$(echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 1)$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 9))
 password=$(echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 1)$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 14))
+# 生成1024-65535之间的随机端口
+port=$(shuf -i 1024-65535 -n 1)
 
 wget https://github.com/yeahwu/image/raw/refs/heads/master/caddy.tar.gz -O - | tar -xz -C /usr/local/
 echo "====输入已经DNS解析好的域名===="
 read domain
-
-isPort=`netstat -ntlp| grep -E ':80 |:443 '`
+isPort=$(netstat -ntlp| grep -E ":$port |:80 |:443 ")
 if [ "$isPort" != "" ];then
    clear
    echo " ================================================== "
-   echo " 80或443端口被占用，请先释放端口再运行此脚本"
+   echo " 80、443或${port}端口被占用，请先释放端口再运行此脚本"
    echo
    echo " 端口占用信息如下："
    echo $isPort
    echo " ================================================== "
    exit 1
 fi
-
 mkdir -p /etc/caddy
 cat >/etc/caddy/https.caddyfile<<EOF
-:443, $domain
+:$port, $domain
 route {
    forward_proxy {
        basic_auth $username $password
@@ -40,7 +38,6 @@ route {
    file_server
 }
 EOF
-
 cat >/etc/systemd/system/caddy.service<<EOF
 [Unit]
 Description=Caddy
@@ -59,32 +56,29 @@ AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 [Install]
 WantedBy=multi-user.target
 EOF
-
 systemctl enable caddy.service && systemctl restart caddy.service && systemctl status --no-pager caddy.service
 rm -f https.sh
-
 cat >/etc/caddy/https.json<<EOF
 {
 ===========配置参数=============
 代理模式：Https正向代理
 地址：${domain}
-端口：443
+端口：${port}
 用户：${username}
 密码：${password}
 ====================================
-http=$domain:443, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https
+http=$domain:$port, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https
 }
 EOF
-
 echo
 echo "安装已经完成"
 echo
 echo "===========Https配置参数============"
 echo
 echo "地址：${domain}"
-echo "端口：443"
+echo "端口：${port}"
 echo "用户：${username}"  
 echo "密码：${password}"
 echo
 echo "========================================="
-echo "http=$domain:443, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https"
+echo "http=$domain:$port, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https"
